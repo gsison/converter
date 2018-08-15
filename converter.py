6f9af6bf2c87854ad5deb086bd421a4058700f34
Gabriel Dominik Sison <gabrieldominiksison@Gabriels-MacBook-Pro.local>
2018-08-15 08:39:55 +0800

    made importable

--- converter.py
+++ converter.py
@@ -57,39 +57,40 @@ def NxtoPgxEdgelist(G, out, nlabels=None, nprops=None,
             pass
 
 
-with open("Jollibee_Stores.csv", 'r') as infile:
-    reader = csv.reader(infile)
-    runs = 0
-    G = nx.MultiDiGraph()
-    for row in reader:
-        if not runs:
-            runs += 1
-            continue
-        nodeattrs = {'code': row[0], 'kind': 'store',
-                     'name': row[1], 'rbu': int(row[2]), 'own': row[3],
-                     'ntype': row[4], 'lat': float(row[5]),
-                     'long': float(row[6]), 'bfast': bool(row[7]),
-                     'emd': bool(row[8]), 'dt': bool(row[9]),
-                     'jeds': bool(row[10]), 'jpp': bool(row[11]),
-                     'bos': bool(row[12])}
-        G.add_node(row[0], **nodeattrs)
+if __name__ == "__main__":
+    with open("Jollibee_Stores.csv", 'r') as infile:
+        reader = csv.reader(infile)
+        runs = 0
+        G = nx.MultiDiGraph()
+        for row in reader:
+            if not runs:
+                runs += 1
+                continue
+            nodeattrs = {'code': row[0], 'kind': 'store',
+                         'name': row[1], 'rbu': int(row[2]), 'own': row[3],
+                         'ntype': row[4], 'lat': float(row[5]),
+                         'long': float(row[6]), 'bfast': bool(row[7]),
+                         'emd': bool(row[8]), 'dt': bool(row[9]),
+                         'jeds': bool(row[10]), 'jpp': bool(row[11]),
+                         'bos': bool(row[12])}
+            G.add_node(row[0], **nodeattrs)
 
-with open('JB_sno_dmatrix_bank.csv', 'r') as infile:
-    reader = csv.reader(infile)
-    runs = 0
-    JBnames = []
-    for row in reader:
-        if not runs:
-            for name in row[4:]:
-                JBnames.append(name.strip())
+    with open('JB_sno_dmatrix_bank.csv', 'r') as infile:
+        reader = csv.reader(infile)
+        runs = 0
+        JBnames = []
+        for row in reader:
+            if not runs:
+                for name in row[4:]:
+                    JBnames.append(name.strip())
+                runs += 1
+                continue
+            name = 'Bank{}'.format(str(runs).zfill(5))
             runs += 1
-            continue
-        name = 'Bank{}'.format(str(runs).zfill(5))
-        runs += 1
-        nodeattrs = {'code': name, 'name': row[0], 'lat': float(row[1]),
-                     'long': float(row[2]), 'ntype': row[3]}
-        G.add_node(name, **nodeattrs)
-        for ind, val in enumerate(row[4:]):
-            val = float(val)
-            G.add_edge(name, JBnames[ind], distance=val, etype='Distance')
-            G.add_edge(JBnames[ind], name, distance=val, etype='Distance')
+            nodeattrs = {'code': name, 'name': row[0], 'lat': float(row[1]),
+                         'long': float(row[2]), 'ntype': row[3]}
+            G.add_node(name, **nodeattrs)
+            for ind, val in enumerate(row[4:]):
+                val = float(val)
+                G.add_edge(name, JBnames[ind], distance=val, etype='Distance')
+                G.add_edge(JBnames[ind], name, distance=val, etype='Distance')
